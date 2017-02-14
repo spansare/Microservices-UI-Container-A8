164b9856df104e5a7a36a41b6ab687b7973422a7
spansare <spansari@in.ibm.com>
2017-02-14 15:40:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,20 @@ require_once("service-discovery.php");
 
 // Get our Catalog API endpoint from Service Discovery
 //$catalogRoute = getAPIRoute("Catalog-API");
-$catalogRoute = "http://ms-catalogAPI-domical-directive.mybluemix.net";
+//$catalogRoute = "http://ms-catalogAPI-domical-directive.mybluemix.net";
 
-//$application = getenv("VCAP_APPLICATION");
+$application = getenv("sgroup_name");
 //echo "\r\napplication:" . $application;
-//$application_json = json_decode($application, true);
-//$applicationURI = $application_json["application_uris"][0];
+$application_json = json_decode($application, true);
+$applicationURI = $application_json["application_uris"][0];
 //echo "\r\napplicationURI:" . $applicationURI;
-//if (substr( $applicationURI, 0, 3 ) === "ui-") {
-//    $catalogHost = "catalog-api-" . substr($applicationURI, 3);
-//} else {
-//    $catalogHost = str_replace("-ui-", "-catalog-api-", $applicationURI);
-//}
+if (substr( $applicationURI, 0, 3 ) === "ui-") {
+    $catalogHost = "catalog-api-" . substr($applicationURI, 3);
+} else {
+    $catalogHost = str_replace("-ui-", "-catalog-api-", $applicationURI);
+}
 //echo "\r\ncatalogHost:" . $catalogHost;    
-//$catalogRoute = "http://" . $catalogHost;
+$catalogRoute = "http://" . $catalogHost;
 
 // Get the products from our Catalog API
 $result = request("GET", $catalogRoute . "/items");
