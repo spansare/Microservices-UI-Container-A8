da9b2e47ae2f9cb3b2b8b08b0d1f18bddc77dba6
spansare <spansari@in.ibm.com>
2017-02-28 00:41:22 +0530

    Update submit-orders.php

--- submit-orders.php
+++ submit-orders.php
@@ -1,22 +1,22 @@
 <?php
 require_once("service-discovery.php");
 $data = file_get_contents('php://input');
-$services = getenv("VCAP_SERVICES");
-$services_json = json_decode($services, true);
+#$services = getenv("VCAP_SERVICES");
+#$services_json = json_decode($services, true);
 
 // Get the orders application url route from service discovery
 //$ordersRoute = getAPIRoute("Orders-API");
 
-$application = getenv("sgroup_name");
-$application_json = json_decode($application, true);
-$applicationName = $application_json["name"];
-$ordersAppName = str_replace("ui-", "orders-api-", $applicationName);
-ordersAppName = str_replace("-ui", "-orders-api", $ordersAppName);
-$applicationURI = $application_json["application_uris"][0];
-$ordersHost = substr_replace($applicationURI, $ordersAppName, 0, strlen($applicationName));
-$ordersRoute = "http://" . $ordersHost;
-$ordersURL = $ordersRoute . "/rest/orders";
-
+#$application = getenv("sgroup_name");
+#$application_json = json_decode($application, true);
+#$applicationName = $application_json["name"];
+#$ordersAppName = str_replace("ui-", "orders-api-", $applicationName);
+#ordersAppName = str_replace("-ui", "-orders-api", $ordersAppName);
+#$applicationURI = $application_json["application_uris"][0];
+#$ordersHost = substr_replace($applicationURI, $ordersAppName, 0, strlen($applicationName));
+#$ordersRoute = "http://" . $ordersHost;
+#$ordersURL = $ordersRoute . "/rest/orders";
+$ordersURL = "http://localhost:6379/orders/JavaOrdersAPI/rest/orders"
 //$ordersURL = $ordersRoute . "/rest/orders";
 //$ordersURL = "http://ms-ordersAPI-hyperfunctional-throstle.mybluemix.net/rest/orders";
 
