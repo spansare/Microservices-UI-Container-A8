610f6be9b7a28e05aac851cda8a76c5c4363062e
spansare <spansari@in.ibm.com>
2017-02-28 00:38:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,19 +6,19 @@ require_once("service-discovery.php");
 //$catalogRoute = getAPIRoute("Catalog-API");
 //$catalogRoute = "http://ms-catalogAPI-domical-directive.mybluemix.net";
 
-$application = getenv("sgroup_name");
+#$application = getenv("sgroup_name");
 //echo "\r\napplication:" . $application;
-$application_json = json_decode($application, true);
-$applicationURI = $application_json["application_uris"][0];
+#$application_json = json_decode($application, true);
+#$applicationURI = $application_json["application_uris"][0];
 //echo "\r\napplicationURI:" . $applicationURI;
-if (substr( $applicationURI, 0, 3 ) === "ui-") {
-    $catalogHost = "catalog-api-" . substr($applicationURI, 3);
-} else {
-    $catalogHost = str_replace("-ui-", "-catalog-api-", $applicationURI);
-}
+#if (substr( $applicationURI, 0, 3 ) === "ui-") {
+#    $catalogHost = "catalog-api-" . substr($applicationURI, 3);
+#} else {
+#    $catalogHost = str_replace("-ui-", "-catalog-api-", $applicationURI);
+#}
 //echo "\r\ncatalogHost:" . $catalogHost;    
-$catalogRoute = "http://" . $catalogHost;
-
+#$catalogRoute = "http://" . $catalogHost;
+$catalogRoute = "http://localhost:6379/catalog-service/items/"
 // Get the products from our Catalog API
 $result = request("GET", $catalogRoute . "/items");
 ?>
